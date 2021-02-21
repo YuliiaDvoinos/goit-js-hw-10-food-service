79d4092c97590f2cb877ea4abe610fe4e9da55df
Yevhenii Blynnyi <v-yblynnyi@expediagroup.com>
2021-02-21 16:08:20 +0200

    added markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles.css';
+import itemsTemplate from './templates/menu-items.hbs';
+import menu from './menu.json'
+
+
+const markup = itemsTemplate(menu);
+const galleryRef = document.querySelector('.js-menu');
+galleryRef.insertAdjacentHTML('beforeend', markup);
